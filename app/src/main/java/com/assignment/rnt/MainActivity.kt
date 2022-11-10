a9b5fcde09c2d623c8e65b35b443e5cdb078903b
GIlyazitdinov Raphael <iraphaele@gmail.com>
2022-11-10 12:44:17 +0800

    Update Material3 theme and colors, added support for DarkTheme and DynamicColor on Android 12+.

--- app/src/main/java/com/assignment/rnt/MainActivity.kt
+++ app/src/main/java/com/assignment/rnt/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.assignment.rnt.ui.theme.RandomNumberTestTheme
+import com.assignment.rnt.ui.theme.SystemUi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RandomNumberTestTheme {
+                SystemUi(window)
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
