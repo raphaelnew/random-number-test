ffec7a8e351963daa7c4627fb4515a2b09a3cd13
GIlyazitdinov Raphael <iraphaele@gmail.com>
2022-11-10 18:23:20 +0800

    Show "Remote Number" after fetched, for better understanding and testing.

--- app/src/main/java/com/assignment/rnt/ui/screen/number/NumberTestScreen.kt
+++ app/src/main/java/com/assignment/rnt/ui/screen/number/NumberTestScreen.kt
@@ -1,9 +1,6 @@
 package com.assignment.rnt.ui.screen.number
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -12,7 +9,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.assignment.rnt.R
+import com.assignment.rnt.ui.theme.RandomNumberTestTheme
 import com.assignment.rnt.ui.viewmodel.LocalNumberUiState
 import com.assignment.rnt.ui.viewmodel.NumberTestViewModel
 import com.assignment.rnt.ui.viewmodel.RemoteNumberUiState
@@ -74,6 +76,18 @@ private fun NumberTestScreen(
                 when (resultUiState) {
                     is ResultUiState.Empty -> {
                         if (numberUiState is RemoteNumberUiState.Loaded) {
+                            Text(
+                                text = stringResource(
+                                    id = R.string.number_test_remote_number,
+                                    numberUiState.number
+                                ),
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth(),
+                                fontWeight = FontWeight.Bold,
+                                style = MaterialTheme.typography.bodyMedium,
+                                textAlign = TextAlign.Center,
+                            )
                             UserInput(
                                 inputUiState = inputUiState,
                                 onValidateInput = onValidateInput,
@@ -93,4 +107,127 @@ private fun NumberTestScreen(
             }
         }
     )
+}
+
+/**
+ * Previews with fake sample data
+ */
+@Preview
+@Composable
+fun NumberTestScreenLoadingLight() {
+    RandomNumberTestTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loading,
+                inputUiState = LocalNumberUiState.Normal(),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenLoadingDark() {
+    RandomNumberTestTheme(isDarkTheme = true) {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loading,
+                inputUiState = LocalNumberUiState.Normal(),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenLoadedLight() {
+    RandomNumberTestTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal(),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenLoadedDark() {
+    RandomNumberTestTheme(isDarkTheme = true) {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal(),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenInputLight() {
+    RandomNumberTestTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal("22"),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenInputDark() {
+    RandomNumberTestTheme(isDarkTheme = true) {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal("22"),
+                resultUiState = ResultUiState.Empty,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenResultLight() {
+    RandomNumberTestTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal("22"),
+                resultUiState = ResultUiState.Compared(
+                    22,
+                    55,
+                    stringResource(id = R.string.result_lower, 22, 55)
+                ),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NumberTestScreenResultDark() {
+    RandomNumberTestTheme(isDarkTheme = true) {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NumberTestScreen(
+                numberUiState = RemoteNumberUiState.Loaded(55),
+                inputUiState = LocalNumberUiState.Normal("22"),
+                resultUiState = ResultUiState.Compared(
+                    22,
+                    55,
+                    stringResource(id = R.string.result_lower, 22, 55)
+                ),
+            )
+        }
+    }
 }
